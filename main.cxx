e2b1f1fe0a9af6048dba69b0aabf83779826c317
martin blick <nuttybread@msn.com>
2020-12-22 17:39:53 +0000

    another commit

--- main.cxx
+++ main.cxx
@@ -10,7 +10,7 @@ main(int argc, char** argv)
 	std::cout << "Hello world!\n";
 
 	InputParser::InputParser ip;
-	ip.readFile("C:\\Users\\kateb\\MartinBlick\\Source\\Repos\\physics-code\\input.txt");
+	ip.readFile("");
 
 	Meshes::Meshes m;
 	Meshes::GridPoints gp(10, Meshes::dimension::D3);
